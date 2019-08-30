76382e3f25309a80738f9d081c3d09c98e367c88
DevinMussman <54691518+DevinMussman@users.noreply.github.com>
2019-08-29 19:27:37 -0500

    Add files via upload

---
+++ hello.c
@@ -0,0 +1,17 @@
+/**
+ * Author: Devin and Brady
+ * Date: 2019/29/08
+ *
+ * a simple hello world program in C
+ *
+ */
+#include<stdlib.h>
+#include<stdio.h>
+int main(int argc, char **argv) {
+
+
+printf("Devin Mussman and Brady Gebbie\n")
+printf("Computer Science and Engineering")
+
+return 0;
+}
